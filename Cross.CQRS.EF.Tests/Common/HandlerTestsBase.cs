673c2c24a6eba7755b93975b746b0464b90d8a14
Denis Peshkov <denis.peshkov@outlook.com>
2025-06-18 10:04:59 +0300

    add template project for Tests

---
+++ Cross.CQRS.EF.Tests/Common/HandlerTestsBase.cs
@@ -0,0 +1,30 @@
+﻿namespace Cross.CQRS.EF.Tests.Common;
+
+[TestFixture]
+public abstract class HandlerTestsBase
+{
+    protected IConfiguration Configuration;
+
+    [SetUp]
+    public virtual void OneTimeSetUp()
+    {
+
+        Configuration = LoadConfiguration();
+    }
+
+    [TearDown]
+    // [OneTimeTearDown]
+    public virtual void OneTimeTearDown()
+    {
+    }
+
+    private static IConfiguration LoadConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Test.json", false, true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+        return builder.Build();
+    }
+}
