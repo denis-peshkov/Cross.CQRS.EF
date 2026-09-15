c70ef1557a633cdf9655e08213b7797655828770
Denis Peshkov <denis.peshkov@outlook.com>
2026-09-15 16:33:24 +0300

    Align polyfill namespaces with `System.Runtime.CompilerServices` for conditional `NET6_0` compatibility.

--- Cross.CQRS.EF.Tests/Polyfills/CompilerFeatureRequiredAttribute.cs
+++ Cross.CQRS.EF.Tests/Polyfills/CompilerFeatureRequiredAttribute.cs
@@ -1,21 +1,23 @@
 ﻿#if NET6_0
-
-namespace Cross.CQRS.EF.Tests.Polyfills;
-
-[AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
-internal sealed class CompilerFeatureRequiredAttribute : Attribute
-{
-    public CompilerFeatureRequiredAttribute(string featureName)
-    {
-        FeatureName = featureName;
-    }
-
-    public string FeatureName { get; }
-
-    public bool IsOptional { get; init; }
-
-    public const string RefStructs = nameof(RefStructs);
-    public const string RequiredMembers = nameof(RequiredMembers);
-}
-
+namespace System.Runtime.CompilerServices
+#else
+namespace Cross.CQRS.EF.Tests.Polyfills
 #endif
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    internal sealed class CompilerFeatureRequiredAttribute : Attribute
+    {
+        public CompilerFeatureRequiredAttribute(string featureName)
+        {
+            FeatureName = featureName;
+        }
+
+        public string FeatureName { get; }
+
+        public bool IsOptional { get; set; }
+
+        public const string RefStructs = nameof(RefStructs);
+
+        public const string RequiredMembers = nameof(RequiredMembers);
+    }
+}
--- Cross.CQRS.EF.Tests/Polyfills/RequiredMemberAttribute.cs
+++ Cross.CQRS.EF.Tests/Polyfills/RequiredMemberAttribute.cs
@@ -1,13 +1,14 @@
 ﻿#if NET6_0
-
-namespace Cross.CQRS.EF.Tests.Polyfills;
-
-[AttributeUsage(
-    AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property,
-    AllowMultiple = false,
-    Inherited = false)]
-internal sealed class RequiredMemberAttribute : Attribute
-{
-}
-
+namespace System.Runtime.CompilerServices
+#else
+namespace Cross.CQRS.EF.Tests.Polyfills
 #endif
+{
+    [AttributeUsage(
+        AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property,
+        AllowMultiple = false,
+        Inherited = false)]
+    internal sealed class RequiredMemberAttribute : Attribute
+    {
+    }
+}
